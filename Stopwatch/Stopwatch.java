94060e665f545328adf53b7df04e6a5d51534754
abhi-pillai <12a.abhishekpillai@gmail.com>
2026-02-09 21:30:17 +0530

    Stopwatch Thread Code

---
+++ Stopwatch/Stopwatch.java
@@ -0,0 +1,47 @@
+package Clock.Stopwatch;
+
+public class Stopwatch implements Runnable {
+    private String startTime;
+    private String elapsedTime;
+    private int laps = 0;
+    boolean running = true;
+    Stopwatch(String startTime) {
+        this.startTime = startTime;
+        this.elapsedTime = "00:00:00:000";
+    }
+    @Override
+    public void run() {
+        long startTime = System.nanoTime();
+
+        while (running) {
+            long elapsedNanos = System.nanoTime() - startTime;
+            long elapsedMillis = elapsedNanos / 1_000_000;
+
+            long centiseconds = (elapsedMillis / 10) % 100;
+            long seconds = (elapsedMillis / 1000) % 60;
+            long minutes = (elapsedMillis / 60000) % 60;
+            long hours = elapsedMillis / 3600000;
+
+            System.out.printf(
+                "\r%02d:%02d:%02d:%02d",
+                hours, minutes, seconds, centiseconds
+            );
+            elapsedTime = String.format("%02d:%02d:%02d:%02d", hours, minutes, seconds, centiseconds);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                System.out.println("Stopwatch interrupted: " + e.getMessage());
+            }
+
+        }
+        System.out.println("\nStopwatch stopped" );
+    }
+
+    public void lap() {
+        laps++;
+        System.out.println("\nLap " + laps + ": " + elapsedTime);
+    }
+    public void stop() {
+        running = false;
+    }
+}
